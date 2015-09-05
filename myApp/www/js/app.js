bde0b0803a6b2cc406b69be3690166f298192ca5
Nelson Taveras <ntaveras03@gmail.com>
2015-09-05 18:01:03 -0400

    Add mock data to main view

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -1,14 +1,42 @@
-angular.module('starter', ['ionic'])
+(function(){
+  var app = angular.module('starter', ['ionic']);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+  app.config(['$stateProvider', function($stateProvider){
+
+  }]);
+
+  app.controller('FirstCtrl', ['$scope', '$log', function($scope, $log){
+    $scope.transactions = [
+      {
+        'title' : 'Pizza at Laurus kajsd kasdkasjdk jdkj askdj aksdjk askdj asjkd',
+        'amount' : 23495.12
+      },
+      {
+        'title' : 'My second transaction',
+        'amount' : 123.45,
+      },
+      {
+        'title' : 'My third and awesome transaction',
+        'amount' : 123456789.99
+      }
+    ];
+    $scope.fn = function(transaction){
+      $log.log(transaction);
+    };
+  }]);
+
+  app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+})();
+
