754bee75fd59d8531bb0847af4912339dd45b62b
Nelson Taveras <ntaveras03@gmail.com>
2015-09-06 20:35:27 -0400

    Add persistency through localStorage

--- myApp/www/js/TransactionsService.js
+++ myApp/www/js/TransactionsService.js
@@ -3,12 +3,17 @@
 var app = angular.module('eTracker');
 
 app.factory('TransactionsService', [function(){
-  var transactions = [
-  ];
+
+  var store = window.localStorage;
+  var transactions = angular.fromJson(store['transactions']) || [];
+  var persist = function(){
+    store['transactions'] = angular.toJson(transactions);
+  };
   return {
     add : function(transaction){
       transaction.id = transactions.length;
       transactions.push(transaction);
+      persist();
     },
     getAll : function(){
       return transactions;
@@ -24,12 +29,14 @@ app.factory('TransactionsService', [function(){
       for(var i = 0; i < transactions.length; ++i){
         if(transactions[i].id == transaction.id){
           transactions[i] = transaction;
+          persist();
           return ;
         }
       }
     },
     delete : function(id){
       transactions.splice(id, 1);
+      persist();
     }
   }
 }]);
